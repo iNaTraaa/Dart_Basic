ce02656334bee0f3fdbd6efa3ec9d3d9ccf8027a
iNaTraaa <donatra07@gmail.com>
2025-12-18 22:38:19 +0700

    Day 1

--- Day1/Lab7.dart
+++ Day1/Lab7.dart
@@ -1,15 +1,15 @@
-void main(){
+void main() {
   Object obj = "Na Tra";
   // Kiểm tra obj có phải là kiểu String không
-  if(obj is String){
+  if (obj is String) {
     print("Obj là một chuỗi String");
   }
   // Kiểm tra obj không phải là kiểu String
-  if(obj is! int){
+  if (obj is! int) {
     print("Obj không phải là một chuỗi số nguyên");
   }
 
-  // Ép kiểu 
+  // Ép kiểu
   String str = obj as String;
   print(str.toUpperCase());
 }
--- Day1/Lab8.dart
+++ Day1/Lab8.dart
@@ -2,8 +2,7 @@ void main() {
   var a = 10;
   print(a);
 
-
-// ??= sẽ gán giá trị bên phải cho biến bên trái nếu biến bên trái đang có giá trị null
+  // ??= sẽ gán giá trị bên phải cho biến bên trái nếu biến bên trái đang có giá trị null
   int? b;
   b ??= 5; // b đang là null nên sẽ được gán giá trị 5
   print(b); // In ra 5
